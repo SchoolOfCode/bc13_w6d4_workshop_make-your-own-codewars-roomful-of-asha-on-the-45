3cad18ce0a26776bc3da85e4e442ce8eed950734
jason_riley <jason.rileycode@hotmail.com>
2022-11-03 15:40:36 +0000

    Kata complete

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 /* 
-👉 Write your kata here!
+👉 Create an array of all prime numbers up until a given number!
+
+If input = 5, we would expect the function to return [2, 3, 5]
+If input = 20, we would expect the function to return [2, 3, 5, 7, 11, 13, 17, 19]
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function generatePrimes(input) {
+    
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,27 @@
+import { test, expect } from "@jest/globals";
+import { generatePrimes } from "./main.js";
+
 //👉 Write your tests below here:
+test("Function returns an array of all prime numbers up to 1", () => {
+    let actual = generatePrimes(1);
+    let expected = [];
+    expect(actual).toEqual(expected);
+})
+
+test("Function returns an array of all prime numbers up to 5", () => {
+    let actual = generatePrimes(5);
+    let expected = [2, 3, 5];
+    expect(actual).toEqual(expected);
+})
+
+test("Function returns an array of all prime numbers up to 10", () => {
+    let actual = generatePrimes(10);
+    let expected = [2, 3, 5, 7];
+    expect(actual).toEqual(expected);
+})
+
+test("Function returns an array of all prime numbers up to 100", () => {
+    let actual = generatePrimes(100);
+    let expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+    expect(actual).toEqual(expected);
+})
